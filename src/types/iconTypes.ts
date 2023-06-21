bfa3899d87914d2f45b08103ed1c3c6687a22ea8
me.luc <lukasalmeida372@gmail.com>
2023-06-20 23:04:56 -0300

    feat: update styles

--- src/types/iconTypes.ts
+++ src/types/iconTypes.ts
@@ -2,3 +2,7 @@ export interface IconProps extends React.SVGProps<SVGSVGElement> {
 	className: string;
 	size?: number;
 }
+
+export interface ToastIconProps extends React.SVGProps<SVGSVGElement> {
+	type: 'success' | 'error' | 'warning' | 'info';
+}
