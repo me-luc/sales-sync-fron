b45b705468dcb905ba0d04b607887ddf9a473916
me.luc <lukasalmeida372@gmail.com>
2023-06-20 23:03:44 -0300

    feat: add new icons

--- src/app/(secure)/products/page.tsx
+++ src/app/(secure)/products/page.tsx
@@ -1,30 +1,52 @@
 'use client';
 import { Product, ProductOverview } from '@/components';
 import { SearchProduct } from '@/components/product/SearchProduct';
+import { ProductsContext } from '@/context/ProductsContext';
 import { useProduct } from '@/hook';
-import { Product as ProductType, ProductParams } from '@/types';
-import { use, useEffect, useState } from 'react';
+import { SelectMultipleIcon, ToastSuccessIcon } from '@/icons';
+import { Product as ProductType } from '@/types';
+import { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 export default function Page() {
+	const [showOverview, setShowOverview] = useState(false);
+	const [editingMultiple, setEditingMultiple] = useState(false);
 	const { products, productsLoading } = useProduct();
 	const [filter, setFilter] = useState('');
 	const [filteredProducts, setFilteredProducts] = useState([]);
+	const { products: productsSelection, setProducts } =
+		useContext(ProductsContext);
 
 	useEffect(() => {
 		const filtered = products.filter((product: ProductType) =>
 			product.name.toLowerCase().includes(filter.toLowerCase())
 		);
 		setFilteredProducts(filtered);
-	}, [filter, products]);
+	}, [filter, products, productsSelection]);
 
 	if (productsLoading) {
 		return <div>Loading...</div>;
 	}
 
+	if (!products.length) {
+		return <div>No products</div>;
+	}
+
 	return (
 		<div>
-			<SearchProduct value={filter} setValue={setFilter} />
+			<TopBar>
+				<SearchProduct value={filter} setValue={setFilter} />
+				<IconButton onClick={editMultiple}>
+					<SelectMultipleIcon />
+				</IconButton>
+			</TopBar>
+
+			{showOverview && (
+				<ProductOverview
+					setShowOverview={setShowOverview}
+					products={products}
+				/>
+			)}
 			<StyledList>
 				{filteredProducts.map((product: ProductType) => (
 					<Product
@@ -35,11 +57,26 @@ export default function Page() {
 						price={Number(product.price)}
 						quantity={product.quantity}
 						image={product.photo}
+						editingMultiple={editingMultiple}
+						setShowOverview={setShowOverview}
 					/>
 				))}
 			</StyledList>
+			{editingMultiple && (
+				<ContinueButton onClick={handleContinue}>
+					<ToastSuccessIcon />
+				</ContinueButton>
+			)}
 		</div>
 	);
+
+	function handleContinue() {
+		setShowOverview(true);
+	}
+
+	function editMultiple() {
+		setEditingMultiple((prev) => !prev);
+	}
 }
 
 const StyledList = styled.ul`
@@ -48,4 +85,31 @@ const StyledList = styled.ul`
 	gap: 10px;
 	grid-template-columns: repeat(auto-fit, 150px);
 	justify-content: center;
+`;
+
+const ContinueButton = styled.button`
+	width: 50px;
+	height: 50px;
+	background: none;
+	border: none;
+	cursor: pointer;
+
+	position: fixed;
+	bottom: 100px;
+	right: 35px;
+`;
+
+const IconButton = styled.button`
+	background: none;
+	border: none;
+`;
+
+const TopBar = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 100%;
+	height: auto;
+
+	margin-bottom: 25px;
 `;
---
+++ src/icons/ToastSuccessIcon.tsx
@@ -0,0 +1,17 @@
+import { ToastIconProps } from '@/types';
+
+export function ToastSuccessIcon() {
+	return (
+		<svg
+			width='55'
+			height='55'
+			viewBox='0 0 55 55'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'>
+			<path
+				d='M27.5 0C12.375 0 0 12.375 0 27.5C0 42.625 12.375 55 27.5 55C42.625 55 55 42.625 55 27.5C55 12.375 42.625 0 27.5 0ZM22 41.25L8.25 27.5L12.1275 23.6225L22 33.4675L42.8725 12.595L46.75 16.5L22 41.25Z'
+				fill='#309D00'
+			/>
+		</svg>
+	);
+}
