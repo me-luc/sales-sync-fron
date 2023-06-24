250521cd952dbd7f02698df73f0e33e161fb306e
me.luc <lukasalmeida372@gmail.com>
2023-06-24 13:20:50 -0300

    feat: add checkout response data

--- src/app/checkout/fail/page.tsx
+++ src/app/checkout/fail/page.tsx
@@ -1,6 +1,9 @@
+import { ToastWarningIcon } from '@/icons';
+
 export default function Home() {
 	return (
 		<div>
+			<ToastWarningIcon type='error' />
 			<h1>Deu algo errado!</h1>
 		</div>
 	);
--- src/app/checkout/layout.tsx
+++ src/app/checkout/layout.tsx
@@ -1,11 +1,28 @@
 'use client';
+import styled from 'styled-components';
 import { ToastContainer } from 'react-toastify';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<div>
+		<Container>
 			{children}
 			<ToastContainer />
-		</div>
+		</Container>
 	);
 }
+
+const Container = styled.div`
+	display: flex;
+	flex-flow: column;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+	height: 100vh;
+	padding: 25px;
+
+	h1 {
+		font-size: 26px;
+		font-weight: 700;
+		margin-top: 20px;
+	}
+`;
